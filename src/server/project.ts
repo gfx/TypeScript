a0f74068a7621d6a4fa13df27c6ac59503f802a3
Sheetal Nandi <shkamat@microsoft.com>
2023-10-03 10:55:12 -0700

    For now make sure module resolution cache usage doesnt go past program creation

--- src/server/project.ts
+++ src/server/project.ts
@@ -833,6 +833,11 @@ export abstract class Project implements LanguageServiceHost, ModuleResolutionHo
     }
 
     /** @internal */
+    getPackageJsonInfoCache() {
+        return this.resolutionCache.getModuleResolutionCache().getPackageJsonInfoCache();
+    }
+
+    /** @internal */
     resolveTypeReferenceDirectiveReferences<T extends string | FileReference>(
         typeDirectiveReferences: readonly T[],
         containingFile: string,
@@ -2624,7 +2629,7 @@ export class AutoImportProviderProject extends Project {
                     hostProject.currentDirectory,
                     compilerOptions,
                     host,
-                    program.getModuleResolutionCache(),
+                    program.getPackageJsonInfoCache(),
                 );
                 if (packageJson) {
                     const entrypoints = getRootNamesFromPackageJson(packageJson, program, symlinkCache);
@@ -2644,7 +2649,7 @@ export class AutoImportProviderProject extends Project {
                             directory,
                             compilerOptions,
                             host,
-                            program.getModuleResolutionCache(),
+                            program.getPackageJsonInfoCache(),
                         );
                         if (typesPackageJson) {
                             const entrypoints = getRootNamesFromPackageJson(typesPackageJson, program, symlinkCache);
@@ -2684,7 +2689,7 @@ export class AutoImportProviderProject extends Project {
                 packageJson,
                 compilerOptions,
                 host,
-                program.getModuleResolutionCache(),
+                program.getPackageJsonInfoCache(),
                 resolveJs,
             );
             if (entrypoints) {
@@ -2835,8 +2840,8 @@ export class AutoImportProviderProject extends Project {
     }
 
     /** @internal */
-    override getModuleResolutionCache() {
-        return this.hostProject.getCurrentProgram()?.getModuleResolutionCache();
+    override getPackageJsonInfoCache() {
+        return this.hostProject.getPackageJsonInfoCache();
     }
 }
 
